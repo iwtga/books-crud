46146858d8b6f4d052a8f70a528433ab413d3f9e
Alan Almeida <alan241015@gmail.com>
2022-05-29 23:54:13 +0530

    add update & delete

--- bookscrud/views.py
+++ bookscrud/views.py
@@ -4,9 +4,10 @@ from bookscrud.models import Books
 
 @app.route('/books/createBook', methods=["POST"])
 def create_book():
-    name = request.json['name']
-    author = request.json['author']
-    description = request.json['description']
+    content = request.json
+    name = content['name']
+    author = content['author']
+    description = content['description']
     book = Books(
         name = name,
         author = author,
@@ -21,3 +22,22 @@ def read_book():
     for book in Books.objects:
         books.append(book.get_json())
     return make_response(jsonify(books), 200)
+
+@app.route('/books/<book_id>', methods=["PUT"])
+def update_book(book_id):
+    content = request.json
+    book = Books.objects(book_id=book_id).first()
+    if book:
+        book.update(name = content["name"], author = content["author"], description = content["description"])
+        return make_response("Book Updated Successfully!", 204)
+    else:
+        return make_response("Invalid Book", 400)
+
+@app.route('/books/<book_id>', methods=["DELETE"])
+def delete_book(book_id):
+    book = Books.objects(book_id=book_id).first()
+    if book:
+        book.delete()
+        return make_response("Book Deleted Successfully!", 204)
+    else:
+        return make_response("Book Invalid!", 400)
